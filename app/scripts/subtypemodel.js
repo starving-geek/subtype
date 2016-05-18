dd632cac2d96969a8a5303244820489a2c26e337
Tyler <tyler@geekingdoing.com>
2016-05-18 05:56:49 -0400

    updated subtype model: changed helper methods

--- app/scripts/subtypemodel.js
+++ app/scripts/subtypemodel.js
@@ -1,6 +1,6 @@
 /*
  * Tyler Deans
- * May 16, 2016
+ * May 18, 2016
  */
 
 
@@ -24,43 +24,70 @@ function getFunction() {
     var randIndex = getRandomInt(0, 3) // random number between 0 and 2
     var functions = [
         'fun makeGreen (p:{color:string}) = p.color = "green"',
+        'val distToOrigin:{x:real, y:real} = ', // complete def
         'fun setX (p:{x:real}) = x = 4.0',
         'fun setY (p:{y:real}) = y = 4.0'
     ];
     return functions[randIndex];
 }
 
-function getStatment(questionType) {
-    var randIndex = getRandomInt(0, 7) // random number between 0 and 7
+function getStatment() {
+    var randIndex = getRandomInt(0, 3) // random number between 0 and 3
     var statements = [
-        'val c:{x:real, y:real} = {x=3.0, y=4.0}',
-        'val c:{color:string} = {color = "brown"}',
-        'val c:{x:real, y:real, color:string} = {x=3.0, y=4.0, color="brown"}',
+
+        'val _ = makeGreen(c)',
         'val _ = c.x',
         'val _ = c.y',
         'val _ = c.f' // runtime error
     ];
-
+    return statements[randIndex];
 }
 
+function getVariable() {
+    var randIndex = getRandomInt(0, 3);
+    var variables = [
+        'val c:{x:real, y:real} = {x=3.0, y=4.0}',
+        'val c:{color:string} = {color = "brown"}',
+        'val c:{x:real, y:real, color:string} = {x=3.0, y=4.0, color="brown"}'
+    ];
+
+    return variables[randIndex];
+}
 function getType() {
+    var randIndex = getRandomInt(0, 6);
     var types = [
         '{x:real, y:real, color:string}', // subtype of {x:real, y:real}
         '{y:real, x:real}', // subtype of {x:real, y:real}
         '{x:real}', // not a subtype
         '{x:string, y:string, color:string}', // not a subtype
-        '{x:int, y:int, z:bool}', // subtype of {x:real, y:real}
-        '{x:int, y:int} ' // A type is a subtype of itself ({x:real, y:real})
+        '{x:real, y:real, z:bool}', // subtype of {x:real, y:real}
+        '{x:real, y:real} ' // A type is a subtype of itself ({x:real, y:real})
     ];
+
+    return types[randIndex];
 }
 
-function getSubtypeExpression() {
+function getSubtypeExpression(questionType) {
     var subtypeString = "";
+
+    if (questionType === "runtime") {
+        var variableType = getType();
+        var statement = getStatment();
+        var variableFunction = getFunction();
+    } else {
+        var type = getType();
+        subtypeString = type;
+    }
+
+    return subtypeString;
 }
 
+function getAnswer(questionType) {
+
+}
 /*
  * Generates the code snippet which will bw displayed on the web page
- * Also calculates the answer to the question based on the question type
+ * Also returns the answer to the question based on the question type
 */
 SubtypeModel.prototype.evalSubtypeExpression = function() {
     this.subtypeExpression = "<pre>" + "\n";
@@ -68,7 +95,7 @@ SubtypeModel.prototype.evalSubtypeExpression = function() {
     this.subtypeExpression += "</pre>"
 
     if (questionType === "subtype") {
-        this.subtypeExpression += "<label id=\"subtypeQuestion\"> Is this a subtype of {x:real, y:real}? True or false?</label>";
+        this.subtypeExpression += "<label id=\"subtypeQuestion\"> Is that a subtype of {x:real, y:real}? True or false?</label>";
     } else {
         this.subtypeExpression += "<label id=\"subtypeQuestion\"> Will this code run? True or false?</label>";
     }
